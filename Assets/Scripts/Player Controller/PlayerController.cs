bdfead96aae34029e39297f9b380538465e37196
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-02-18 12:00:11 -0500

    more energy animation work

--- Assets/Scripts/Player Controller/PlayerController.cs
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour
     public TextMeshProUGUI energyText;
     public int currEnergy;
     public int maxEnergy;
+    public ToggleEnergyGlow toggleEnergyGlow;
 
     public void decreaseCurrEnergy(int energyUsed)
     {
         currEnergy = currEnergy - energyUsed;
         energyText.text = currEnergy.ToString() + "/" + maxEnergy.ToString();
+        if(currEnergy == 0)
+        {
+            toggleEnergyGlow.stopGlow();
+        }
     }
 }
---
+++ Assets/Scripts/Player Controller/ToggleEnergyGlow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleEnergyGlow : MonoBehaviour
+{
+    private new ParticleSystem particleSystem;
+    public void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    public void stopGlow()
+    {
+        particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+    }
+}
