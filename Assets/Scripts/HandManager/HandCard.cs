21cf038f186cd750965323a6fc9da2702bcce479
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-03-05 16:34:11 -0500

    moving card

---
+++ Assets/Scripts/HandManager/HandCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HandCard
+{
+    public Transform transform;
+    public float speed;
+    public Transform endpointTransform;
+
+    public HandCard(Transform transform, float speed, Transform endpointTransform)
+    {
+        this.transform = transform;
+        this.speed = speed;
+        this.endpointTransform = endpointTransform;
+    }
+}
---
+++ Assets/Scripts/HandManager/HandManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    public List<HandCard> handCards;
+    public Transform testTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        foreach(HandCard handCard in handCards)
+        {
+            Vector2 handCardPos = new Vector2(handCard.transform.position.x, handCard.transform.position.y);
+            handCard.transform.position = Vector2.MoveTowards(handCardPos, handCard.endpointTransform.position, handCard.speed * Time.deltaTime);
+        }
+    }
+
+    public void addCardToHand(GameObject card, float speed)
+    {
+        HandCard newHandCard = new HandCard(card.transform, speed, testTransform);
+        handCards.Add(newHandCard);
+    }
+}
