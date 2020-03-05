8059c9d118fd6de2337e106d69527fdcd7274e4f
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-03-05 17:06:14 -0500

    movement improved with scaling and stopping

--- Assets/Scripts/HandManager/HandCard.cs
+++ Assets/Scripts/HandManager/HandCard.cs
@@ -8,11 +8,20 @@ public class HandCard
     public Transform transform;
     public float speed;
     public Transform endpointTransform;
+    public Vector3 endpointScale;
 
     public HandCard(Transform transform, float speed, Transform endpointTransform)
     {
         this.transform = transform;
         this.speed = speed;
         this.endpointTransform = endpointTransform;
+    }
+
+    public HandCard(Transform transform, float speed, Transform endpointTransform, Vector3 endpointScale)
+    {
+        this.transform = transform;
+        this.speed = speed;
+        this.endpointTransform = endpointTransform;
+        this.endpointScale = endpointScale;
     }
 }
--- Assets/Scripts/HandManager/HandManager.cs
+++ Assets/Scripts/HandManager/HandManager.cs
@@ -24,13 +24,40 @@ public class HandManager : MonoBehaviour
         foreach(HandCard handCard in handCards)
         {
             Vector2 handCardPos = new Vector2(handCard.transform.position.x, handCard.transform.position.y);
-            handCard.transform.position = Vector2.MoveTowards(handCardPos, handCard.endpointTransform.position, handCard.speed * Time.deltaTime);
+            bool changeOccurred = false;
+            if (!positionsAreTheSame(handCard.transform, handCard.endpointTransform))
+            {
+                handCard.transform.position = Vector2.MoveTowards(handCardPos, handCard.endpointTransform.position, handCard.speed * Time.deltaTime);
+                changeOccurred = true;
+            } 
+            if (handCard.endpointScale != null && !scalesAreTheSame(handCard.transform.localScale, handCard.endpointScale))
+            {
+                handCard.transform.localScale = new Vector3(handCard.transform.localScale.x + .01f, handCard.transform.localScale.y + .01f, handCard.transform.localScale.z);
+                changeOccurred = true;
+            }
+            if(!changeOccurred)
+            {
+                handCards.Remove(handCard);
+            }
         }
     }
 
     public void addCardToHand(GameObject card, float speed)
     {
-        HandCard newHandCard = new HandCard(card.transform, speed, testTransform);
+        HandCard newHandCard = new HandCard(card.transform, speed, testTransform, new Vector3(.2f, .2f, 1));
         handCards.Add(newHandCard);
+    }
+
+    private bool positionsAreTheSame(Transform t1, Transform t2)
+    {
+        Vector2 pos1 = new Vector2(t1.transform.position.x, t1.transform.position.y);
+        Vector2 pos2 = new Vector2(t2.transform.position.x, t2.transform.position.y);
+        return pos1 == pos2;
+    }
+    private bool scalesAreTheSame(Vector3 t1, Vector3 t2)
+    {
+        Vector2 scale1 = new Vector2(t1.x, t1.y);
+        Vector2 scale2 = new Vector2(t2.x, t2.y);
+        return scale1 == scale2;
     }
 }
