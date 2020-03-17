d55fdc09ca94b84693cc7f3c68ed8e10e9a00c35
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-03-17 19:09:28 -0400

    beginning of attack cards

---
+++ Assets/Card Template/Scripts/AttackCard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AttackCard")]
+public class AttackCard : Card
+{
+    public int attackModifier;
+    public string attackText;
+}
