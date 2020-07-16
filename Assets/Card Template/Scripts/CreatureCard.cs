08b12b5fbef5f99416e6d3998130f3cd33f2fbec
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2020-07-16 11:13:51 -0400

    crisper ui

--- Assets/Card Template/Scripts/CreatureCard.cs
+++ Assets/Card Template/Scripts/CreatureCard.cs
@@ -7,12 +7,16 @@ public class CreatureCard : Card
 {
     public int attack;
     public int currAttack;
+    public int thisTurnAttack;
 
     public int defense;
     public int currDefense;
+    public int thisTurnDefense;
 
     public int maxHealth;
     public int currHealth;
+
+    public bool isExhausted;
 
     public Sprite circleArtwork;
 
